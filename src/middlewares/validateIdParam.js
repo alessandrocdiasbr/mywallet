cb688e60c7ba8f94072dc0e23587e08a7c9817d2
alessandrocdiasbr <acdias83@gmail.com>
2025-01-12 17:41:53 -0300

    correção de código

--- src/middlewares/validateIdParam.js
+++ src/middlewares/validateIdParam.js
@@ -1,9 +1,11 @@
+import { ObjectId } from 'mongodb';
+
 export function validateIdParam(req, res, next) {
     const { id } = req.params;
     
-    if (!id || isNaN(id)) {
+    if (!ObjectId.isValid(id)) {
         return res.status(400).json({ message: "ID inválido" });
     }
     
     next();
-} 
+}
