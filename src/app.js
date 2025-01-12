8d5e5fee35694ca37b81a55d4765d55d772f17ba
alessandrocdiasbr <acdias83@gmail.com>
2025-01-11 22:01:25 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(transactionRouter);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
 });
